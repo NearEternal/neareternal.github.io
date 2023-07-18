67ddb8e2448f01d5798141e9d8ae2cd9182888b2
NearEternal <132073300+NearEternal@users.noreply.github.com>
2023-07-18 18:38:38 +0300

    Update Advanced Sensing2.js

--- ScratchExtensions/Advanced Sensing2.js
+++ ScratchExtensions/Advanced Sensing2.js
@@ -47,7 +47,7 @@ class AdvancedSensing {
         }
       ],
       menus: {
-            spriteMenu: ['donamic 1', 'somee', '3'] //'getSpriteNames'  {text:"press",value:1}, {text:"release",value:0}
+            spriteMenu: 'getSpriteNames' //Working --> ['donamic 1', 'somee', '3'] {text:"press",value:1}, {text:"release",value:0}
         }
     }
   }
